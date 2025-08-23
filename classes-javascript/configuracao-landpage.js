b4dac5d9b6b6ff00e6a931dad9b6e61059697a6d
digitalltech-solutions <digitalltech100@gmail.com>
2025-08-23 08:15:49 -0300

    Atualizar

--- classes-javascript/configuracao-landpage.js
+++ classes-javascript/configuracao-landpage.js
@@ -23,6 +23,10 @@ function eventoModoDark() {
             elementosDark.style.backgroundColor = '#303030'
         })
 
+        window.document.querySelectorAll('.configurar-fundo-dark-dois').forEach((elementosDark) => {
+            elementosDark.style.backgroundColor = '#242424'
+        })
+
         window.document.querySelectorAll('.configurar-icone-dark').forEach((elementosDark) => {
             elementosDark.style.color = '#303030'
             elementosDark.style.display = 'block'
@@ -84,6 +88,7 @@ function eventoModoDark() {
         window.document.getElementById('imagem-carrossel-dois').src = '../recursos/imagens/card-mensagem-dois-dark.jpg'
         window.document.getElementById('imagem-carrossel-tres').src = '../recursos/imagens/card-mensagem-tres-dark.jpg'
         window.document.getElementById('icone-ecommerce').src = '../recursos/imagens/icone-ecommerce-claro.png'
+        window.document.getElementById('icone-empresa-acessibilidade').src = '../recursos/imagens/icone-ecommerce-claro.png'
         window.document.getElementById('input-comentario').style.backgroundColor = 'black'
         window.document.getElementById('nome-usuario').style.backgroundColor = 'black'
 
@@ -102,6 +107,10 @@ function eventoModoDarkDois() {
     if(caixa1.checked == true){
         window.document.querySelectorAll('.configurar-fundo-dark').forEach((elementosDark) => {
             elementosDark.style.backgroundColor = '#303030'
+        })
+
+        window.document.querySelectorAll('.configurar-fundo-dark-dois').forEach((elementosDark) => {
+            elementosDark.style.backgroundColor = '#242424'
         })
         
         window.document.querySelectorAll('.configurar-icone-dark').forEach((elementosDark) => {
@@ -160,10 +169,11 @@ function eventoModoDarkDois() {
             botoes.style.color = '#ffffff'
         })
 
-        window.document.getElementById('icone-mobile').src = '../recursos/imagens/icone-digitalltech-mobile-dark.jpg'
         window.document.getElementById('imagem-carrossel-um').src = '../recursos/imagens/card-mensagem-um-dark.jpg'
         window.document.getElementById('imagem-carrossel-dois').src = '../recursos/imagens/card-mensagem-dois-dark.jpg'
+        window.document.getElementById('icone-empresa-acessibilidade').src = '../recursos/imagens/icone-ecommerce-claro.png'
         window.document.getElementById('imagem-carrossel-tres').src = '../recursos/imagens/card-mensagem-tres-dark.jpg'
+        window.document.getElementById('icone-mobile').src = '../recursos/imagens/icone-digitalltech-mobile-dark.jpg'
         window.document.getElementById('icone-ecommerce').src = '../recursos/imagens/icone-ecommerce-claro.png'
         window.document.getElementById('input-comentario').style.backgroundColor = 'black'
         window.document.getElementById('nome-usuario').style.backgroundColor = 'black'
